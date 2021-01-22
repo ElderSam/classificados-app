7f8ba062dac5b11d8b64cee6f8fe0e93922058f7
ElderSam <eldersamuel98@gmail.com>
2021-01-21 22:53:29 -0300

    component FooterList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import ClassifiedList from './components/ClassifiedList';
+import FooterList from './components/FooterList';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <ClassifiedList />
+      <FooterList />
     </div>
   );
 }
---
+++ src/components/FooterList.js
@@ -0,0 +1,11 @@
+import './FooterList.css';
+
+function FooterList() {
+    return(
+        <div class='div-grey' id='footer-list'>
+            5 classificados
+        </div>
+    );
+}
+
+export default FooterList;
