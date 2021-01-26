f8f617158d6ed0761380e66f168ec6efab70cde2
ElderSam <eldersamuel98@gmail.com>
2021-01-25 23:17:52 -0300

    Add an entity model

---
+++ backend/ClassifiedsAPI/ClassifiedsAPI/Models/Classified.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ClassifiedsAPI.Models
+{
+    public class Classified
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("title")] // The Title property is annotated with the [BsonElement] attribute. The attribute's value of 'title' represents the property title in the MongoDB collection.
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+
+    }
+}
