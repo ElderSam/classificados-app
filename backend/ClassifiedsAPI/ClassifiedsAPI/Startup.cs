02fd993d6d4d960ac69099251e59e0466375eb89
ElderSam <eldersamuel98@gmail.com>
2021-01-25 23:24:19 -0300

    Add a configuration model

--- backend/ClassifiedsAPI/ClassifiedsAPI/Startup.cs
+++ backend/ClassifiedsAPI/ClassifiedsAPI/Startup.cs
@@ -1,3 +1,4 @@
+using ClassifiedsAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -26,6 +28,12 @@ namespace ClassifiedsAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<ClassifiedstoreDatabaseSettings>(
+                Configuration.GetSection(nameof(ClassifiedstoreDatabaseSettings)));
+
+            services.AddSingleton<IClassifiedstoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<ClassifiedstoreDatabaseSettings>>().Value);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
