eda18e8370b9faaf7d8f8292b12470322484f29f
ElderSam <eldersamuel98@gmail.com>
2021-01-25 23:32:28 -0300

    Add a CRUD operations service

--- backend/ClassifiedsAPI/ClassifiedsAPI/Startup.cs
+++ backend/ClassifiedsAPI/ClassifiedsAPI/Startup.cs
@@ -1,18 +1,12 @@
 using ClassifiedsAPI.Models;
+using ClassifiedsAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ClassifiedsAPI
 {
@@ -34,6 +28,8 @@ namespace ClassifiedsAPI
 
             services.AddSingleton<IClassifiedstoreDatabaseSettings>(sp =>
                 sp.GetRequiredService<IOptions<ClassifiedstoreDatabaseSettings>>().Value);
+
+            services.AddSingleton<ClassifiedService>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
