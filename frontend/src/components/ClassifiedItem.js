687a71a41f95078166b69ec1b983d4153601286f
ElderSam <eldersamuel98@gmail.com>
2021-01-27 22:33:08 -0300

    formating data to pt-br (Portuguese)

--- frontend/src/components/ClassifiedItem.js
+++ frontend/src/components/ClassifiedItem.js
@@ -4,11 +4,18 @@ import "./ClassifiedItem.css";
 function ClassifiedItem({ classified, index }) {
   const { title, description, date } = classified;
 
+  const year = date.substr(0, 4)
+  const month = parseInt(date.substr(5, 2))
+  const day = date.substr(8, 2)
+
+  const arrMonths = new Array ('janeiro', 'fevereiro', 'março', 'abril', 'Maio', 'junho', 'agosto', 'outubro', 'novembro', 'dezembro');
+  const dateAux = `${day} de ${arrMonths[month]} de ${year}`;
+
   return (
     <>
       <div class="div-grey card item">
         <h3>{title}</h3>
-        <label>{date}</label>
+        <label>{dateAux}</label>
         <p>{description}</p>
       </div>
     </>
