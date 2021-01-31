f1752dbd04b5173230fb958beef0304958e71b4f
ElderSam <eldersamuel98@gmail.com>
2021-01-31 13:22:42 -0300

    Redux: update Reducer and Actions - GET ALL, GET BY ID, CREATE, UPDATE and DELETE

--- frontend/src/store/reducers/classifiedsReducer.js
+++ frontend/src/store/reducers/classifiedsReducer.js
@@ -1,7 +1,8 @@
-import { GET_CLASSIFIEDS, CLASSIFIEDS_ERROR } from '../types';
+import { GET_CLASSIFIEDS, GET_CLASSIFIEDBYID, CREATE, UPDATE, DELETE, CLASSIFIEDS_ERROR } from '../types';
 
 const InitialState = {
     classifieds: [],
+    classified: {},
     loading: true
 };
 
@@ -11,7 +12,35 @@ export default function reducer(state = InitialState, action) {
         case GET_CLASSIFIEDS:
             return {
                 ...state,
-                classifieds: action.payload,
+                classifieds: action.payload, // [{title: 'title', description: 'description...'}, {...}]
+                loading: false
+            }
+
+        case GET_CLASSIFIEDBYID:
+            return {
+                ...state,
+                classified: action.payload, // {title: 'title', description: 'description...'}
+                loading: false
+            }
+
+        case CREATE:
+            return {
+                ...state,
+                classifieds: [...state.classifieds, action.payload],
+                loading: false
+            }
+
+        case UPDATE:
+            return {
+                ...state,
+                classifieds: state.classifieds.map(x => x.id === action.payload.id ? action.payload : x),
+                loading: false
+            }
+        
+        case DELETE:
+            return {
+                ...state,
+                classifieds: state.classifieds.filter(x => x.id !== action.payload),
                 loading: false
             }
 
